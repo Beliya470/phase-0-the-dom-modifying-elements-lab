16a011f10013dfdefee4d2a33f19d4e2a35da706
Beliya470 <anneanziay78@gmail.com>
2023-06-24 07:56:10 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Write your code here!
+// Remove the 'main' element
+const main = document.getElementById("main");
+if (main) {
+  main.remove();
+}
+
+// Create a 'newHeader' element
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.textContent = "YOUR-NAME is the champion"; // Replace YOUR-NAME with your actual name
+
+// Append 'newHeader' to the document body
+document.body.appendChild(newHeader);
